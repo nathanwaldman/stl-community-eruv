9c61de045067c9e6b878aedf97081e783b3a4a64
nathanwaldman <nathanwaldman@users.noreply.github.com>
2026-08-31 01:05:37 -0500

    making it clearer in the printable map that the wash u hillel is in the eruv

--- docs/shuls.js
+++ docs/shuls.js
@@ -1,14 +1,14 @@
-const shul = (lat, long, name, url) => ({lat, long, name, url});
+const shul = (lat, long, name, url, printOffset = null) => ({ lat, long, name, url, printOffset });
 
 // Coordinates converted from shul addresses using https://gps-coordinates.org/coordinate-converter.php
 window.SHULS = [
-    shul(38.6678606,    -90.3658123,    "Nusach Hari B'nai Zion",       "https://nhbz.org/"),
-    shul(38.6601935,    -90.3489994,    "Agudas Israel of St. Louis",   "https://www.agudastl.com/"),
-    shul(38.6600257,    -90.3478443,    "Congregation Bais Menachem",   "https://www.chabadstl.org/templates/section_cdo/aid/7005899/jewish/CBM.htm"),
-    shul(38.660503,     -90.346561,     "Young Israel of St. Louis",    "https://yistl.org/"),
-    shul(38.6604933,    -90.3368608,    "U. City Shul",                 "https://www.ucityshul.org/"),
-    shul(38.6518679,    -90.3343675,    "Kol Rinah",                    "https://www.kolrinahstl.org/"),
-    shul(38.6471789,    -90.3167333,    "Chabad on Campus",             "https://www.chabadwashu.org/"),
-    shul(38.6562585,    -90.3118002,    "Bais Abraham Congregation",    "https://www.baisabe.com/"),
-    shul(38.6457806,    -90.3037566,    "Wash U Hillel",                "https://www.washuhillel.org/"),
+    shul(38.6678606, -90.3658123, "Nusach Hari B'nai Zion", "https://nhbz.org/"),
+    shul(38.6601935, -90.3489994, "Agudas Israel of St. Louis", "https://www.agudastl.com/", [-34, -24]),
+    shul(38.6600257, -90.3478443, "Congregation Bais Menachem", "https://www.chabadstl.org/templates/section_cdo/aid/7005899/jewish/CBM.htm", [0, 34]),
+    shul(38.660503, -90.346561, "Young Israel of St. Louis", "https://yistl.org/", [30, 20]),
+    shul(38.6604933, -90.3368608, "U. City Shul", "https://www.ucityshul.org/"),
+    shul(38.6518679, -90.3343675, "Kol Rinah", "https://www.kolrinahstl.org/"),
+    shul(38.6471789, -90.3167333, "Chabad on Campus", "https://www.chabadwashu.org/"),
+    shul(38.6562585, -90.3118002, "Bais Abraham Congregation", "https://www.baisabe.com/"),
+    shul(38.6457806, -90.3037566, "Wash U Hillel", "https://www.washuhillel.org/", [-42, -28]),
 ];
